56a46c3d044b37e4aabee78826b7221580d421bf
silvertae <76121068+silvertae@users.noreply.github.com>
2023-06-23 11:08:14 +0900

    [FE] 1주차 구현 PR (#31)
    * [FE] chore #1: 개발환경 세팅  (#2)
    
    * [#1] feat: CRA 세팅
    
    * [#1] chore: lint 세팅
    
    * [#1] chore: eslint 세팅
    
    * [#1] chore: prettier 세팅
    
    * [FE] feat #1: main 상단탭 구현 (#20)
    
    * [#15] design: 메인화면 레이아웃 구성
    
    * [#15] design: 상단 탭 메뉴 레이아웃 구성
    
    * [#17] feat: 상단 탭메뉴 구현중
    
    * [#17] fix: 탭메뉴 버그 해결
    
    - 드래그가 안되던 문제 해결
    - css 너비 조정
    
    * [#15] design: reset css적용
    
    * [#15] design: 폰트 세팅
    
    * [#15] design: 상단 탭 메뉴 레이아웃 구성
    
    * [FE] feat #5:상단탭 & 메인화면 UI 및 기능 작업 (#27)
    
    * design: 메인화면의 메인영역 UI 구현
    
    * design: 메인영역 카드 컴포넌트들 디자인 수정 및 간단한 호버 효과 추가
    
    * feat: 상단탭에서 클릭된 탭 배경색 변경 기능 추가
    
    - TabContainer에 activeTabIndex state 추가
    - key를 props를 사용해서 발생한 오류 해결
    
    * feat: 각 메뉴 클릭시 해당하는 상품들 렌더링 기능 추가
    
    - 더미 데이터 menuList를 만들어서 구현
    
    * feat: MSW 설치 후 mock server에서 데이터를 받아오는 기능 구현
    
    - MSW 패키지 설치
    - mock server와 API 명세서에 맞는 mock data 구성
    - 서버에서 fetch한 데이터로 렌더링하도록 컴포넌트 수정
    - 상품 이미지 assets에 추가
    
    * fix: MSW 관련 오류 해결
    
    - 처음 페이지 로드될 떄 뜨는 에러 해결
    - msw 패키지 1.2.1로 다운그레이드
    - msw 패키지 다운그레이드를 하기 위해서 typescript 5.0.4로 다운그레이드
    
    * design: 상단 탭메뉴 활성화 시 테두리 색상이 변경되도록 수정
    
    * [FE] feat #23: 메뉴 담기 화면 모달 구현 (#29)
    
    * [#23] chore: 메뉴 담기 화면 라우팅 세팅
    
    * [#24] design: 메뉴 담기 모달창 UI 구현 및 임시 기능 구현
    
    - 더미 메뉴 카드 클릭시 모달창 띄우기 구현
    - 수량 조절 기능 구현
    - 수량은 1에서 내려가지 않음
    
    * [#28]  feat: 메뉴 담기 모달창 기능 추가 구현
    
    - 사이즈, 온도, 수량 조정 기능
    
    ---------
    
    Co-authored-by: silvertae <76121068+silvertae@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: gunoc <86706366+gunoc@users.noreply.github.com>

---
+++ fe/src/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import App from './App';
+
+import './reset.css';
+import './index.css';
+import { TabMenu } from './components/TabMenu';
+import { TestBtnForModal } from './components/TestBtnForModal';
+import { AddMenu } from './components/AddMenu';
+import { Modal } from './components/Modal';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/add-menu/:id',
+        element: <AddMenu />,
+      },
+    ],
+  },
+]);
+
+import { worker } from './mocks/browser';
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    {/* <App />
+     */}
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
