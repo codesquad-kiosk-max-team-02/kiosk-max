fee4f77a34bf0c44de7e775994f99f19036a147b
DOEKYONG <apochmn@naver.com>
2023-06-28 04:38:16 +0900

    [be] feat #59 : Orders 테이블 Insert,날짜 비교 후 주문번호 초기화 구현

--- be/src/main/java/com/codesquad/kiosk/repository/OrderRepository.java
+++ be/src/main/java/com/codesquad/kiosk/repository/OrderRepository.java
@@ -1,7 +1,46 @@
 package com.codesquad.kiosk.repository;
 
+import com.codesquad.kiosk.domain.Order;
+import com.codesquad.kiosk.domain.OrderMenu;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
+@RequiredArgsConstructor
 public class OrderRepository {
+
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public Optional<Order> getOrder() {
+        String sql = "SELECT ORDER_TIME, ORDER_NUMBER FROM ORDERS WHERE id = (SELECT MAX(id) FROM ORDERS)";
+        List<Order> list = namedParameterJdbcTemplate.query(sql,orderRowMapper());
+        return list.stream().findFirst();
+    }
+
+    @Transactional
+    public void saveOrder(int orderNumber) {
+        // Orders Insert
+        String insertOrderQuery = "INSERT INTO ORDERS(ORDER_NUMBER) VALUES(:order_number)";
+        SqlParameterSource orderParameters = new MapSqlParameterSource("order_number", orderNumber);
+        KeyHolder orderKeyHolder = new GeneratedKeyHolder();
+        namedParameterJdbcTemplate.update(insertOrderQuery, orderParameters, orderKeyHolder);
+    }
+
+    private RowMapper<Order> orderRowMapper () {
+        return (rs, rowNum) ->
+                Order.builder()
+                     .orderTime(rs.getString("ORDER_TIME"))
+                     .orderNumber(rs.getInt("ORDER_NUMBER"))
+                     .build();
+    }
 }
--- be/src/main/java/com/codesquad/kiosk/service/OrderService.java
+++ be/src/main/java/com/codesquad/kiosk/service/OrderService.java
@@ -1,12 +1,52 @@
 package com.codesquad.kiosk.service;
 
+import com.codesquad.kiosk.domain.Order;
+import com.codesquad.kiosk.domain.OrderMenu;
+import com.codesquad.kiosk.dto.OrderNumberCreatorDto;
+import com.codesquad.kiosk.dto.OrderRequestDto;
 import com.codesquad.kiosk.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
 
     private final OrderRepository orderRepository;
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+
+    public void saveOrder(OrderRequestDto orderRequestDto) {
+        String now = createNowDateformat();
+        Order order = orderRepository.getOrder().orElse(Order.builder().orderTime(now).orderNumber(0).build());
+        OrderNumberCreatorDto dto = new OrderNumberCreatorDto(order.getOrderTime(),order.getOrderNumber());
+        orderRepository.saveOrder(createOrderNumber(dto,now));
+
+    }
+
+
+
+    private int createOrderNumber(OrderNumberCreatorDto dto,String now) {
+        String storedDate = dto.getDate();
+        int orderNumber = dto.getOrderNumber();
+        String currentDate = storedDate.split(" ")[0];
+        if(!now.equals(currentDate)) {
+            return 1;
+        }
+        return orderNumber+1;
+    }
+
+    private String createNowDateformat() {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDate date = now.toLocalDate();
+        String formattedDate = date.format(formatter);
+        return formattedDate;
+    }
+
 }
