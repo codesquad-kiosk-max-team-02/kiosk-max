1ef2bb3f352530b8b1db78ad172505e1a30a0fe5
he2joo <hottestej6@gamil.com>
2023-06-28 12:18:58 +0900

    [BE] feat #66: 메뉴담기 화면 조회 service 생

--- be/src/main/java/com/codesquad/kiosk/service/MenuService.java
+++ be/src/main/java/com/codesquad/kiosk/service/MenuService.java
@@ -1,6 +1,8 @@
 package com.codesquad.kiosk.service;
 
 import com.codesquad.kiosk.dto.CategoryResponseDto;
+import com.codesquad.kiosk.dto.MenuDetailDto;
+import com.codesquad.kiosk.dto.OptionCategoryDto;
 import com.codesquad.kiosk.repository.MenuRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,5 +21,33 @@ public class MenuService {
                 .map(CategoryResponseDto::new)
                 .collect(Collectors.toList());
         return categoryResponseList;
+    }
+
+    public MenuDetailDto getMenuDetail(Integer menuId) {
+        MenuDetailDto menuDetailDto = menuRepository.getMenuDetail(menuId);
+        if (menuDetailDto == null) {
+            throw new IllegalArgumentException();
+        }
+        return updateOptionPrice(menuDetailDto);
+    }
+
+    private MenuDetailDto updateOptionPrice(MenuDetailDto menuDetailDto) {
+        List<OptionCategoryDto> optionList = menuDetailDto.getOption();
+
+        List<String> optionCategoryTypes = optionList.stream()
+                .map(OptionCategoryDto::getOptionCategoryType)
+                .collect(Collectors.toList());
+
+        List<OptionCategoryDto> updatedOptionList =  optionList.stream()
+                .map(option -> {long count = optionCategoryTypes.stream()
+                        .filter(type -> type.equals(option.getOptionCategoryType()))
+                        .count();
+                if (count == 1) {
+                option.setOptionPrice(0);}
+                return option;
+                })
+                .collect(Collectors.toList());
+        menuDetailDto.setOption(updatedOptionList);
+        return menuDetailDto;
     }
 }
