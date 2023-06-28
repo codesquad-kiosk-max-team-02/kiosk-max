b510dfcaa2a25cb6974222f99232825a871ca97d
sejeong00 <buttermelt@daum.net>
2023-06-28 14:43:06 +0900

    [BE] feat #69: 영수증 조회 api 구현

--- be/src/main/java/com/codesquad/kiosk/service/OrderService.java
+++ be/src/main/java/com/codesquad/kiosk/service/OrderService.java
@@ -1,12 +1,21 @@
 package com.codesquad.kiosk.service;
 
+import com.codesquad.kiosk.dto.CategoryResponseDto;
+import com.codesquad.kiosk.dto.ReceiptDto;
 import com.codesquad.kiosk.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
 
     private final OrderRepository orderRepository;
+
+    public ReceiptDto getReceiptByOrderId(Integer orderId) {
+        return orderRepository.getReceiptByOrderId(orderId);
+    }
 }
